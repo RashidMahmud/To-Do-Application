2ac92cbf91a40f8616101cfc8fb6500230942bce
Rashid Mahmud <rashidmahmud999@gmail.com>
2025-11-21 01:30:23 +0600

    To-do button

--- index.js
+++ index.js
@@ -2,3 +2,13 @@
 const input = document.getElementById('todo-input');
 const addBtn = document.getElementById('add-btn');
 const todoList = document.getElementById('todo-list');
+
+                                                        // To-do button
+
+addBtn.addEventListener('click', () => {
+  const text = input.value.trim();
+  if (text !== "") {
+    addTodoItem(text);
+    input.value = "";
+  }
+});
